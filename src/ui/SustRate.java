a5f5a21cce4b63cb1b957fe82d528ba5ad8ed281
juancholkovich <judhenaosa@unal.edu.co>
2017-12-09 20:18:19 -0500

    Reading fasta

--- src/ui/SustRate.java
+++ src/ui/SustRate.java
@@ -1,12 +1,27 @@
 package ui;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class SustRate {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	static FileReader reader;
+	
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		String line;
+		
+		BufferedReader buffer = new BufferedReader(reader = new FileReader("/home/juan/soft/SustRate/test.txt"));
+		
+		while((line = buffer.readLine()) != null){
+			System.out.println(line);
+		}
 
 	}
 
