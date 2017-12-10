e569dc0e6c22202d8596f56ad27441733d06d649
juancholkovich <judhenaosa@unal.edu.co>
2017-12-10 00:04:06 -0500

    Init methods

--- src/logic/CalcRate.java
+++ src/logic/CalcRate.java
@@ -1,19 +1,33 @@
 package logic;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 
 public class CalcRate {
 	
 	private ArrayList<String> joiner;
-	private ArrayList<Object> data = new ArrayList<Object>();
+	private Hashtable<String,Hashtable> data = new Hashtable<String,Hashtable>();
+	private int aCounter, tCounter;
 
 	public CalcRate(ArrayList<String> joiner){
 		this.setJoiner(joiner);
 	}
 	
 	public void dataCreater(){
-		for(int i = 0; i < joiner.size(); i++){
-			System.out.println(joiner.get(i));
+		for(int i = 0; i < joiner.size(); i+=2){
+			aCounter = 0;
+			tCounter = 0;
+			for(int j = 0; j < joiner.get(i+1).length(); j++){
+				char variable = joiner.get(i+1).charAt(j);
+				switch(variable){
+					case 'A':
+						aCounter++;
+						break;
+					case 'T':
+						tCounter++;
+						break;
+				}
+			}
 		}
 	}
 	
